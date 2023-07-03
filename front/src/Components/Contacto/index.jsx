6c2eefbbd2a25308b87aeb6805f42a57386eaaf1
ojitos369 <ojitos369@gmail.com>
2023-07-03 09:24:09 -0600

    idk

--- front/src/Components/Contacto/index.jsx
+++ front/src/Components/Contacto/index.jsx
@@ -16,90 +16,9 @@ const Contacto = () => {
 
     return (
         <Fragment>
-            {/* Contact */}
-      <section id="contact-us" className="contact-us section">
-        <div className="container">
-          <div className="contact-head">
-            <div className="row">
-              <div className="col-lg-8 col-12">
-                <div className="form-main">
-                  <div className="title">
-                    <h4>Ponerse en contacto</h4>
-                    <h3>Escribenos un mensaje</h3>
-                  </div>
-                  <form className="form" method="post" action="mail/mail.php">
-                    <div className="row">
-                      <div className="col-lg-6 col-12">
-                        <div className="form-group">
-                          <label htmlFor="name">Tu nombre<span>*</span></label>
-                          <input id="name" name="name" type="text" placeholder="" required />
-                        </div>
-                      </div>
-                      <div className="col-lg-6 col-12">
-                        <div className="form-group">
-                          <label htmlFor="subject">Su asunto<span>*</span></label>
-                          <input id="subject" name="subject" type="text" placeholder="" required />
-                        </div>
-                      </div>
-                      <div className="col-lg-6 col-12">
-                        <div className="form-group">
-                          <label htmlFor="email">Su correo electronico<span>*</span></label>
-                          <input id="email" name="email" type="email" placeholder="" required />
-                        </div>
-                      </div>
-                      <div className="col-lg-6 col-12">
-                        <div className="form-group">
-                          <label htmlFor="phone">Su telefono<span>*</span></label>
-                          <input id="phone" name="phone" type="text" placeholder="" required />
-                        </div>
-                      </div>
-                      <div className="col-12">
-                        <div className="form-group message">
-                          <label htmlFor="message">Tu mensaje<span>*</span></label>
-                          <textarea id="message" name="message" placeholder="" required autoComplete="off" />
-                        </div>
-                      </div>
-                      <div className="col-12">
-                        <div className="form-group button">
-                          <button type="submit" className="btn btn-primary">Enviar mensaje</button>
-                        </div>
-                      </div>
-                    </div>
-                  </form>
-                </div>
-              </div>
-              <div className="col-lg-4 col-12">
-                <div className="single-head">
-                  <div className="single-info">
-                    <i className="fas fa-phone" />
-                    <h4 className="title">Llamanos ahora:</h4>
-                    <ul>
-                      <li>+51 956-789-110</li>
-                      <li>+51 972-452-112</li>
-                    </ul>
-                  </div>
-                  <div className="single-info">
-                    <i className="fas fa-envelope-open" />
-                    <h4 className="title">Email:</h4>
-                    <ul>
-                      <li><a href="mailto:info@yourwebsite.com">info@ybelleza.com</a></li>
-                      <li><a href="mailto:support@yourwebsite.com">unfo@web.com</a></li>
-                    </ul>
-                  </div>
-                  <div className="single-info">
-                    <i className="fas fa-location-arrow" />
-                    <h4 className="title">Nuestra direccion:</h4>
-                    <ul>
-                      <li>62/1, Trujillo Victor Larco.</li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className="flex flex-col w-full">
+            
           </div>
-        </div>
-      </section>
-      {/*/ End Contact */}
         </Fragment>
     )
 }
